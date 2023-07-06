9c120af583487178f6ffc86a29a6531f9e8207b4
patrictobola <tobola92@gmail.com>
2023-07-06 16:09:26 +0200

    fixed major bug in card. now cards are same size

--- src/components/ProductionCard/HoverCard.vue
+++ src/components/ProductionCard/HoverCard.vue
@@ -3,20 +3,28 @@ export default {
     props: {
         vote: Number,
         originalTitle: String,
-        overview: String
+        overview: String,
+        language: String,
     },
     data() {
         return {
             hasItHover: false,
-            summary: this.overview.slice(0, 100) + "..."
+            summary: this.overview.slice(0, 100) + "...",
         }
     },
     computed: {
-
         roundedVote() {
             const vote = this.vote;
             return Math.round(vote) / 2;
-        }
+        },
+        hasFlag() {
+            const availableFlags = ["it", "en"];
+            return availableFlags.includes(this.language)
+        },
+        flagPath() {
+            const url = new URL(`../../assets/img/${this.language}.png`, import.meta.url);
+            return url.href;
+        },
     },
 
 }
@@ -24,8 +32,10 @@ export default {
 
 
 <template>
-    <div class="card hover" @mouseenter="hasItHover = true" @mouseleave="hasItHover = false">
+    <div class="card hover">
         <h1>{{ originalTitle }}</h1>
+        <img v-if="hasFlag" :src="flagPath" :alt="originalTitle">
+        <span v-else>Language: {{ language }}</span>
         <span>Vote: {{ roundedVote }}</span>
         <p>{{ summary }}</p>
     </div>
@@ -33,6 +43,17 @@ export default {
 
 
 <style scoped>
+.card {
+    border: 1px solid black;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #111111;
+    color: white;
+}
+
 p {
     padding: 0 10px;
     font-size: 0.9rem;
@@ -41,5 +62,11 @@ p {
 h1 {
     margin: auto 0;
     text-align: center;
+    font-size: 1.5rem;
+}
+
+img {
+    width: 30px;
+    margin: 10px 0;
 }
 </style>
--- src/components/ProductionCard/ProductionCard.vue
+++ src/components/ProductionCard/ProductionCard.vue
@@ -36,22 +36,8 @@ export default {
         :originalTitle="originalTitle" />
 
 
-    <HoverCard v-else @mouseleave="hasItHover = false" :originalTitle="originalTitle" :overview="overview" :vote="vote" />
+    <HoverCard v-else @mouseleave="hasItHover = false" :originalTitle="originalTitle" :language="language"
+        :overview="overview" :vote="vote" />
 </template>
 
-<style scoped>
-.card {
-    border: 1px solid black;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #111111;
-    color: white;
-}
-
-.card.hover {
-    border: 1px solid white;
-}
-</style>
+<style></style>
